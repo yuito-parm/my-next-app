0d385934910bb618938d7bbd1b8afde6ffd5ea61
yuito-kijima <pmppmppmppmppmp@yahoo.co.jp>
2025-06-11 15:09:30 +0900

    React/Next.js：mapで複数Greetingコンポーネント表示

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,15 @@ import Image from "next/image";
 import Greeting from "./components/Greeting";
 
 export default function Home() {
+  const names = ["ハリネズミ", "フクロウ", "フェネック"];
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main>
         <h1>Next.jsはじめました!</h1>
-        <Greeting name="ゆいと" />
+        {names.map((name, i) => (
+          <Greeting key={i} name={name} />
+        ))}
       </main>
     </div>
   );
