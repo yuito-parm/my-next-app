0b9888150e3c6ae9fb32c25178d1f5f08370fbc8
yuito-kijima <pmppmppmppmppmp@yahoo.co.jp>
2025-06-11 15:14:06 +0900

    React/Next.js：useStateでカウンター実装

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,18 @@
+"use client";
+
 import Image from "next/image";
 import Greeting from "./components/Greeting";
+import { useState } from "react";
 
 export default function Home() {
-  const names = ["ハリネズミ", "フクロウ", "フェネック"];
+  const [count, setCount] = useState(0);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main>
         <h1>Next.jsはじめました!</h1>
-        {names.map((name, i) => (
-          <Greeting key={i} name={name} />
-        ))}
+        <p>回数: {count}</p>
+        <button onClick={() => setCount(count + 1)}>+1</button>
       </main>
     </div>
   );
