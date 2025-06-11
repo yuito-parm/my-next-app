443fde16cf8cd1a85afd3c3bf954d34a2e944a6b
yuito-kijima <pmppmppmppmppmp@yahoo.co.jp>
2025-06-11 15:53:58 +0900

    React/Next.js：useStateで入力欄＋リスト追加

--- src/app/page.js
+++ src/app/page.js
@@ -5,14 +5,30 @@ import Greeting from "./components/Greeting";
 import { useState } from "react";
 
 export default function Home() {
-  const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+  const [animals, setAnimals] = useState([]);
 
+  function handleAdd() {
+    if (input.trim() === "") return;
+      setAnimals([...animals, input]);
+      setInput("")
+  }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main>
-        <h1>Next.jsはじめました!</h1>
-        <p>回数: {count}</p>
-        <button onClick={() => setCount(count + 1)}>+1</button>
+        <h1>動物リスト</h1>
+        <input
+          type="text"
+          value={input}
+          onChange={e => setInput(e.target.value)}
+          placeholder="動物名を入力"
+        />
+        <button onClick={handleAdd}>追加</button>
+        <ul>
+          {animals.map((a, i) => (
+            <li key={i}>{a}</li>
+          ))}
+        </ul>
       </main>
     </div>
   );
